c94ac824a76eaff8c7ec44ff5b2ca515a8898a94
Bavo Lesy <bavo.lesy@student.uantwerpen.be>
2022-04-03 12:43:16 +0200

    v3 - Added the right IP address of host1

--- src/main/java/BankingAppSpringBoot/BankingClient/BankingClient.java
+++ src/main/java/BankingAppSpringBoot/BankingClient/BankingClient.java
@@ -9,27 +9,37 @@ public class BankingClient {
     //HERE WE TEST OUR SERVER / CLIENT
     static String getBalance(String user) throws UnirestException {
         String balance;
-        String localURL = "http://localhost:8080/BankingServer/users/" + user;
-        balance = Unirest.get(localURL).asString().getBody();
+        //String localURL = "http://localhost:8080/BankingServer/users/" + user;
+        //balance = Unirest.get(localURL).asString().getBody();
+        String URL = "http://192.168.80.3:8080/BankingServer/users/" + user;
+        balance = Unirest.get(URL).asString().getBody();
         return balance;
     }
     static String putBalance(String user, int money) throws UnirestException {
         String balance;
-        String localURL = "http://localhost:8080/BankingServer/users/" + user;
-        Unirest.put(localURL).header("Content-Type", "application/json").body(Integer.toString(money)).asString();
-        balance = Unirest.get(localURL).asString().getBody();
+        //String localURL = "http://localhost:8080/BankingServer/users/" + user;
+        //Unirest.put(localURL).header("Content-Type", "application/json").body(Integer.toString(money)).asString();
+        //balance = Unirest.get(localURL).asString().getBody();
+        String URL = "http://192.168.80.3:8080/BankingServer/users/" + user;
+        Unirest.put(URL).header("Content-Type", "application/json").body(Integer.toString(money)).asString();
+        balance = Unirest.get(URL).asString().getBody();
         return balance;
     }
     static String newUser(String user, int money) throws UnirestException {
         String balance;
-        String localURL = "http://localhost:8080/BankingServer/users/" + user;
-        Unirest.post(localURL).header("Content-Type", "application/json").body(Integer.toString(money)).asString();
-        balance = Unirest.get(localURL).asString().getBody();
+        //String localURL = "http://localhost:8080/BankingServer/users/" + user;
+        //Unirest.post(localURL).header("Content-Type", "application/json").body(Integer.toString(money)).asString();
+        //balance = Unirest.get(localURL).asString().getBody();
+        String URL = "http://192.168.80.3:8080/BankingServer/users/" + user;
+        Unirest.post(URL).header("Content-Type", "application/json").body(Integer.toString(money)).asString();
+        balance = Unirest.get(URL).asString().getBody();
         return balance;
     }
     static void deleteUser(String user) throws UnirestException {
-        String localURL = "http://localhost:8080/BankingServer/users/" + user;
-        Unirest.delete(localURL).asString();
+        //String localURL = "http://localhost:8080/BankingServer/users/" + user;
+        //Unirest.delete(localURL).asString();
+        String URL = "http://192.168.80.3:8080/BankingServer/users/" + user;
+        Unirest.delete(URL).asString();
     }
     public static void main(String[] args) throws IOException, UnirestException {
         String name;
